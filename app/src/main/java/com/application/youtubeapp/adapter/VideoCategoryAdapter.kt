c1cf40da66bcbfbefcda04837944572d352a44f2
sheikh-20 <sheikhzs1032@gmail.com>
2023-12-31 10:31:42 +0530

    video category adapter integrated

--- app/src/main/java/com/application/youtubeapp/adapter/VideoCategoryAdapter.kt
+++ app/src/main/java/com/application/youtubeapp/adapter/VideoCategoryAdapter.kt
@@ -1,4 +1,36 @@
 package com.application.youtubeapp.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.application.youtubeapp.databinding.VideoCategoryViewBinding
 import com.application.youtubeapp.domain.model.VideoCategory
+
+class VideoCategoryAdapter: ListAdapter<VideoCategory, VideoCategoryAdapter.VideoCategoryViewHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<VideoCategory>() {
+        override fun areItemsTheSame(oldItem: VideoCategory, newItem: VideoCategory): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: VideoCategory, newItem: VideoCategory): Boolean {
+            return oldItem.snippet?.title == newItem.snippet?.title
+        }
+    }
+
+    class VideoCategoryViewHolder(private var binding: VideoCategoryViewBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(videoCategory: VideoCategory) {
+            binding.btnTitle.text = videoCategory.snippet?.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoCategoryViewHolder {
+        return VideoCategoryViewHolder(VideoCategoryViewBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: VideoCategoryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
--- app/src/main/java/com/application/youtubeapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/application/youtubeapp/presentation/home/HomeFragment.kt
@@ -8,16 +8,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.application.youtubeapp.R
+import com.application.youtubeapp.adapter.VideoCategoryAdapter
 import com.application.youtubeapp.common.Resource
 import com.application.youtubeapp.databinding.FragmentHomeBinding
 import com.application.youtubeapp.presentation.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private val homeViewModel: HomeViewModel by viewModels()
+
+    private lateinit var adapter: VideoCategoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,16 +36,20 @@ class HomeFragment : Fragment() {
 
         homeViewModel.getVideoCategory()
 
+        adapter = VideoCategoryAdapter()
+        binding.rvVideoCategory.adapter = adapter
+
         homeViewModel.videoCategory.observe(viewLifecycleOwner) {
             when (it) {
                 is Resource.Loading -> {
-                    binding.text.text = "Loading"
+                    binding.progressBar.show()
                 }
                 is Resource.Failure -> {
-                    binding.text.text = it.throwable.message
+                    binding.progressBar.hide()
                 }
                 is Resource.Success -> {
-                    binding.text.text = it.data.toString()
+                    binding.progressBar.hide()
+                    adapter.submitList(it.data)
                 }
             }
         }
